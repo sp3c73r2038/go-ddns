f3b9b43556de24143062e0e562c7bad7cb9c8289
aleiphoenix <a@momoka.net>
2019-07-30 09:28:29 +0800

    add TXT support

--- cmd/ddns-tool/main.go
+++ cmd/ddns-tool/main.go
@@ -6,7 +6,10 @@ import (
 	"io/ioutil"
 	"log"
 	"reflect"
+	"regexp"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/aleiphoenix/go-ddns/pkg/ddns"
 	"github.com/aleiphoenix/go-ddns/tools/input"
@@ -31,9 +34,11 @@ func askFor(name string, v interface{}) {
 
 func main() {
 
+	var domain = flag.String("domain", "", "whole domain")
 	var hostname = flag.String("hostname", "", "hostname")
 	var zone = flag.String("zone", "", "zone")
-	var ip = flag.String("ip", "", "ip")
+	var recordType = flag.String("type", "A", "DNS record type")
+	var payload = flag.String("payload", "", "payload")
 	var ttl = flag.Int("ttl", 300, "ttl in seconds")
 	var nameserver = flag.String("nameserver", "", "nameserver")
 	var keyFile = flag.String(
@@ -41,14 +46,25 @@ func main() {
 
 	flag.Parse()
 
-	if len(*hostname) <= 0 {
-		askFor("hostname", hostname)
+	if len(*domain) > 0 {
+		re, _ := regexp.Compile(
+			`([-a-z0-9\.]+)\.([-a-z0-9]+\.[-a-z0-9]+)`)
+		groups := re.FindAllStringSubmatch(*domain, -1)
+		if groups == nil {
+			panic(fmt.Sprintf("invalid domain: %s", *domain))
+		}
+		zone = &groups[len(groups)-1][2]
+		hostname = &groups[len(groups)-1][1]
+	} else {
+		if len(*hostname) <= 0 {
+			askFor("hostname", hostname)
+		}
+		if len(*zone) <= 0 {
+			askFor("zone", zone)
+		}
 	}
-	if len(*zone) <= 0 {
-		askFor("zone", zone)
-	}
-	if len(*ip) <= 0 {
-		askFor("ip", ip)
+	if len(*payload) <= 0 {
+		askFor("payload", payload)
 	}
 	if len(*nameserver) <= 0 {
 		askFor("nameserver", nameserver)
@@ -56,7 +72,8 @@ func main() {
 
 	log.Printf("hostname: %s", *hostname)
 	log.Printf("zone: %s", *zone)
-	log.Printf("ip: %s", *ip)
+	log.Printf("payload: %s", *payload)
+	log.Printf("type: %s", *recordType)
 	log.Printf("ttl: %d", *ttl)
 	log.Printf("nameserver: %s", *nameserver)
 
@@ -73,14 +90,32 @@ func main() {
 		tisg[t.FQDN] = t.Key
 	}
 
-	ok, err := ddns.Update(
-		*hostname, *zone, *ip,
-		uint32(*ttl), *nameserver, 3000, tisg)
-	if err != nil {
-		log.Fatal(err)
+	var ok bool
+	timeout := time.Second * 10
+	switch strings.ToLower(*recordType) {
+	case "a":
+		ok, err = ddns.Update(
+			*hostname, *zone, *payload,
+			uint32(*ttl), *nameserver, timeout, tisg)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "txt":
+		txt := []string{*payload}
+		ok, err = ddns.UpdateTXT(
+			*hostname, *zone, txt,
+			uint32(*ttl), *nameserver, timeout, tisg)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	default:
+		log.Fatalf("unknown type: %s", recordType)
 	}
+
 	if ok {
 		fqdn := fmt.Sprintf("%s.%s", *hostname, *zone)
-		log.Printf("%s updated to %s", fqdn, *ip)
+		log.Printf("%s updated to %s", fqdn, *payload)
 	}
+
 }
--- pkg/ddns/dns.go
+++ pkg/ddns/dns.go
@@ -47,7 +47,8 @@ func Query(domain string, server string, timeout int) (rv []string, err error) {
 
 func Update(
 	hostname string, zone string, ip string, ttl uint32,
-	server string, timeout int, tsig map[string]string) (rv bool, err error) {
+	server string, timeout time.Duration,
+	tsig map[string]string) (rv bool, err error) {
 
 	rv = false
 
@@ -83,7 +84,71 @@ func Update(
 
 	c := new(dns.Client)
 	c.Dialer = &net.Dialer{
-		Timeout: time.Millisecond * time.Duration(timeout),
+		Timeout: timeout,
+	}
+
+	if tsig != nil && len(tsig) > 0 {
+		c.TsigSecret = tsig
+		for k, _ := range tsig {
+			m.SetTsig(k, dns.HmacMD5, 300, time.Now().Unix())
+		}
+	}
+
+	// log.Println(m)
+
+	in, _, err := c.Exchange(m, server)
+	if err != nil {
+		return
+	}
+
+	if in.Rcode != dns.RcodeSuccess {
+		err = fmt.Errorf("Rcode: %d", in.Rcode)
+		return
+	}
+	rv = true
+	return
+}
+
+func UpdateTXT(
+	hostname string, zone string, txt []string, ttl uint32,
+	server string, timeout time.Duration,
+	tsig map[string]string) (rv bool, err error) {
+
+	rv = false
+
+	fqdn := dns.Fqdn(fmt.Sprintf("%s.%s", hostname, zone))
+
+	// DELETE TXT RRset first
+	oldRRs := make([]dns.RR, 1)
+	oldRR := new(dns.TXT)
+	oldRR.Hdr = dns.RR_Header{
+		Name:   fqdn,
+		Rrtype: dns.TypeTXT,
+		Class:  dns.ClassANY,
+	}
+	oldRRs[0] = oldRR
+
+	newRRs := make([]dns.RR, 1)
+	newRR := new(dns.TXT)
+	newRR.Hdr = dns.RR_Header{
+		Name:   fqdn,
+		Rrtype: dns.TypeTXT,
+		Class:  dns.ClassINET,
+		Ttl:    ttl,
+	}
+	newRR.Txt = txt
+	newRRs[0] = newRR
+
+	m := new(dns.Msg)
+	m.Id = dns.Id()
+
+	m.SetUpdate(dns.Fqdn(zone))
+	m.RemoveName(oldRRs)
+	m.Insert(newRRs)
+
+	c := new(dns.Client)
+	c.Dialer = &net.Dialer{
+		Timeout: timeout,
 	}
 
 	if tsig != nil && len(tsig) > 0 {
